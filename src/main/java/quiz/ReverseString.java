4dad29a9a9938c0c909f9659ddb8297c08401bc9
Samuel Huang <lydebacle@gmail.com>
2017-12-20 10:44:20 -0500

    Add files via upload
    Updated ReverseString method.

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -2,6 +2,17 @@ package quiz;
 
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+    	String output="";
+    	for(int i=(input.length())-1; i>-1;i--) {
+    		if(i == input.length()-1) {
+    			output = output.concat(input.substring(i));
+    		}
+    		else {
+    			output = output.concat(input.substring(i, i+1));
+    		}
+    	}
+        return output;
     }
+    
+   
 }
